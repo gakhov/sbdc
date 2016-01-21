467deadf2c63cf446be0657ded65e9b064ddf7cf
Andrii Gakhov <gakhov@ferret-go.com>
2016-01-21 15:47:54 +0100

    Add BBC News dataset
    BBC News dataset consists of 2225 documents
    from the BBC news website corresponding to stories
    in five topical areas from 2004-2005.

---
+++ sbdc/datasets/bbc.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import os
+import glob
+import pandas as pd
+
+from .utils import dowload
+
+"""
+BBC News dataset
+
+This dataset consists of 2225 documents from the BBC news website
+corresponding to stories in five topical areas from 2004-2005.
+
+Natural Classes: 5 (business, entertainment, politics, sport, tech)
+
+Official website: http://mlg.ucd.ie/datasets/bbc.html
+
+D. Greene and P. Cunningham.
+Practical Solutions to the Problem of Diagonal Dominance
+in Kernel Document Clustering", Proc. ICML 2006.
+"""
+
+DATASET_CLASSES = set([
+    'business',
+    'entertainment',
+    'politics',
+    'sport',
+    'tech'
+])
+DATASET_URL = "http://mlg.ucd.ie/files/datasets/bbc-fulltext.zip"
+
+
+def load(classes=None):
+    """Load BBC News dataset.
+
+    Read more with the http://mlg.ucd.ie/datasets/bbc.html
+
+    Parameters
+    ----------
+    classes: iterable or None
+        List of required classes to load. If None, all classes will be loaded.
+
+    Returns
+    -------
+    documents: pandas.DataFrame
+        Return DataFrame with 3 columns: title, text, class
+
+    """
+
+    path = dowload(DATASET_URL, "bbc")
+
+    classes = DATASET_CLASSES & set(classes) if classes else DATASET_CLASSES
+    if not classes:
+        return None
+
+    documents = []
+    for cls in classes:
+        files = os.path.join(path, "bbc", cls, "*.txt")
+        for filepath in glob.glob(files):
+            with open(filepath) as f:
+                title, text = f.read().split("\n", 1)
+                documents.append([title.strip(), text.strip(), cls])
+
+    return pd.DataFrame(documents, columns=["title", "text", "class"])
---
+++ sbdc/datasets/utils/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import os
+import urllib
+import zipfile
+
+"""
+Datasets Utils
+
+"""
+
+
+def dowload(origin, folder):
+    """Download and (if needed) extract file from the origin to the folder
+
+    Parameters
+    ----------
+    origin: str
+        Origin URL of the file to download
+
+    folder: str
+        Relative path of the folder where to store the result.
+
+    Returns
+    -------
+    path: str
+        Absolute path where result is stored
+
+    """
+    sbdc_home = os.path.expanduser(os.path.join('~', '.sbdc'))
+    if not os.access(sbdc_home, os.W_OK):
+        sbdc_home = os.path.join('/tmp', '.sbdc')
+
+    datasets_home = os.path.join(sbdc_home, 'datasets')
+    if not os.path.exists(datasets_home):
+        os.makedirs(datasets_home)
+
+    path = os.path.join(datasets_home, folder)
+    if os.path.exists(path):
+        return path
+
+    filehandle, _ = urllib.urlretrieve(origin)
+
+    print filehandle
+
+    zip_file_object = zipfile.ZipFile(filehandle, 'r')
+    print zip_file_object.extractall(path)
+
+    return path
